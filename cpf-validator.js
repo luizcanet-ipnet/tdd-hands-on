43b8d272660fbbd88fa503d8b3f8fcb9f9fd4802
Luiz Canet <luiz.canet@ipnet.cloud>
2026-02-06 15:43:30 -0300

    feat: implement 11-digit validation and custom error class
    - Add CpfValidationError class for domain-specific validation errors
    - Implement private method #has11Digits to verify if the input has exactly 11 digits
    - Update isValid method to include length validation
    - Add a new test case to ensure the validator throws CpfValidationError for invalid lengths

--- cpf-validator.js
+++ cpf-validator.js
@@ -1,6 +1,14 @@
+export class CpfValidationError extends Error {
+    constructor(...params) {
+        super(...params);
+        this.name = "CpfValidationError";
+    }
+}
+
 export default class CpfValidator {
     isValid(cpf) {
         this.#isString(cpf);
+        this.#has11Digits(cpf);
 
         return true;
     }
@@ -8,6 +16,12 @@ export default class CpfValidator {
     #isString(cpf) {
         if (typeof cpf !== "string") {
             throw new TypeError("O CPF deve ser um texto");
+        }
+    }
+
+    #has11Digits(cpf) {
+        if (!/^\d{11}$/.test(cpf)) {
+            throw new CpfValidationError("O CPF deve conter 11 dígitos");
         }
     }
 }
--- cpf-validator.test.js
+++ cpf-validator.test.js
@@ -1,5 +1,5 @@
 import { test } from "./test/lib/test.js";
-import CpfValidator from "./cpf-validator.js";
+import CpfValidator, { CpfValidationError } from "./cpf-validator.js";
 
 test("Deve lançar um erro de validação de tipo quando o valor passado não for um texto", () => {
     const cpfValidator = new CpfValidator();
@@ -17,3 +17,20 @@ test("Deve lançar um erro de validação de tipo quando o valor passado não fo
 
     return false;
 });
+
+test("Deve lançar um erro de validação de CPF quando o valor não contiver 11 dígitos", () => {
+    const cpfValidator = new CpfValidator();
+
+    try {
+        cpfValidator.isValid("123456789A");
+    } catch (error) {
+        if (
+            error instanceof CpfValidationError &&
+            error.message === "O CPF deve conter 11 dígitos"
+        ) {
+            return true;
+        }
+    }
+
+    return false;
+})
